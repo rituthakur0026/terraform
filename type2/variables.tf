2fc32de179f964a3a3bedf58a1ea9682592c189e
rituthakur0026 <166488828+rituthakur0026@users.noreply.github.com>
2025-11-16 12:42:52 +0530

    Add Terraform variables for project configuration

---
+++ type2/variables.tf
@@ -0,0 +1,44 @@
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for subnet"
+  type        = string
+}
+
+variable "az" {
+  description = "Availability Zone"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
